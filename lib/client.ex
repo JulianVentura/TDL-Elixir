203eabd44b39c0bc4280ffd5b0c183680a49d866
guidobotta <guido.botta@tiendanube.com>
2022-11-05 17:39:35 -0300

    Reorganize code

--- lib/client.ex
+++ lib/client.ex
@@ -2,21 +2,11 @@ defmodule Client do
   def game_loop() do
     command =
       IO.gets(_helper_text())
-      |> String.trim
+      |> String.trim()
       |> String.split(" ")
-      |> Enum.map(&(String.to_atom(&1)))
 
     _process_command(command)
     game_loop()
-  end
-
-  defp _process_command(command) do
-    case command do
-      [:attack, enemy] -> IO.puts("Al ataqueeeee, morite #{enemy}")
-      [:move, room] -> IO.puts("Movete perri, vamo a la sala #{room}")
-      [:exit] -> exit("Gracias por jugar")
-      _ -> IO.puts("Comando inválido, intenta otra vez")
-    end
   end
 
   defp _helper_text() do
@@ -28,5 +18,26 @@ defmodule Client do
 
     Ingresa un comando:
     """
+  end
+
+  defp _process_command(command) do
+    case command do
+      ["attack", enemy] -> _attack(enemy)
+      ["move", room] -> _move(room)
+      ["exit"] -> _exit()
+      _ -> IO.puts("Comando inválido, intenta otra vez")
+    end
+  end
+
+  defp _attack(enemy) do
+    IO.puts("Al ataqueeeee, morite #{enemy}")
+  end
+
+  defp _move(room) do
+    IO.puts("Movete perri, vamo a la sala #{room}")
+  end
+
+  defp _exit() do
+    exit("Gracias por jugar")
   end
 end
