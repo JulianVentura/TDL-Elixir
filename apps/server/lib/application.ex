f70f976ea85b9f6567646a5458722a0dad7b69ba
FrancoBar <fbarreneche@fi.uba.ar>
2022-11-09 17:45:52 -0300

    World from Agent to GenServer

--- apps/server/lib/application.ex
+++ apps/server/lib/application.ex
@@ -2,8 +2,8 @@ defmodule Server.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-
   use Application
+  require World
 
   @impl true
   def start(_type, _args) do
--- apps/server/lib/server/world.ex
+++ apps/server/lib/server/world.ex
@@ -1,91 +1,73 @@
 defmodule World do
   require Room
+  use GenServer
 
-  defmodule State do
-    defstruct [:pid_to_label, :label_to_pid, :graph]
+  # Public API
 
-    @type t() :: %__MODULE__{
-            pid_to_label: map | nil,
-            label_to_pid: map | nil,
-            graph: :digraph.t() | nil
-          }
+  def start_link(opts \\ []) do
+    {_, world} = GenServer.start_link(__MODULE__, :ok, opts)
+    world
   end
 
-  use Agent
+  def get_starting_room(world) do
+    GenServer.call(world, :get_starting_room)
+  end
 
-  @type world_pid :: pid | atom
-  @type room_pid :: pid | atom
-  @type direction :: atom
-  @type state_attribute :: map | map | :digraph.t()
-  @type key :: atom
+  def get_neighbours(world, room) do
+    GenServer.call(world, {:get_neighbours, room})
+  end
 
-  @spec start_link() :: pid
-  def start_link() do
-    nil_state = %State{pid_to_label: nil, label_to_pid: nil, graph: nil}
-    {:ok, pid} = Agent.start_link(fn -> nil_state end)
+  def get_neighbours(world, room, direction) do
+    GenServer.call(world, {:get_neighbours, room, direction})
+  end
 
-    gr = :digraph.new()
+  # Handlers
 
-    :digraph.add_vertex(gr, "A")
-    :digraph.add_vertex(gr, "B")
-    :digraph.add_vertex(gr, "C")
-    :digraph.add_edge(gr, "A", "B", :N)
-    :digraph.add_edge(gr, "B", "A", :S)
-    :digraph.add_edge(gr, "A", "C", :S)
-    :digraph.add_edge(gr, "C", "A", :N)
+  @impl true
+  def init(:ok) do
+    world = self()
+    graph = :digraph.new()
+    :digraph.add_vertex(graph, "A")
+    :digraph.add_vertex(graph, "B")
+    :digraph.add_vertex(graph, "C")
+    :digraph.add_edge(graph, "A", "B", :N)
+    :digraph.add_edge(graph, "B", "A", :S)
+    :digraph.add_edge(graph, "A", "C", :S)
+    :digraph.add_edge(graph, "C", "A", :N)
 
     {pid_to_label, label_to_pid} = Enum.reduce(["A", "B", "C"], {%{}, %{}},
       fn label, acc ->
         {pid_to_label, label_to_pid} = acc
-        room_pid = Room.start_link(pid)
+        room_pid = Room.start_link(world)
         label_to_pid = Map.put(label_to_pid, label, room_pid)
         pid_to_label = Map.put(pid_to_label, room_pid, label)
         {pid_to_label, label_to_pid}
       end)
-    
-    state = %State{pid_to_label: pid_to_label, label_to_pid: label_to_pid, graph: gr}
-    _update_state(pid, state)
-    pid
+
+    {:ok, {graph, pid_to_label, label_to_pid}}
   end
 
-  @spec get_starting_room(world_pid) :: room_pid
-  def get_starting_room(world) do
-    %{
-      pid_to_label: _,
-      label_to_pid: label_to_pid,
-      graph: _
-    } = _get_state(world)
-    Map.get(label_to_pid, "A")
+  @impl true
+  def handle_call(:get_starting_room, _from, {graph, pid_to_label, label_to_pid}) do
+    {:reply, Map.get(label_to_pid, "A"), {graph, pid_to_label, label_to_pid}}
   end
 
-  @spec get_neighbours(world_pid, room_pid) :: list(room_pid)
-  def get_neighbours(world, room) do
-    %{
-      pid_to_label: pid_to_label,
-      label_to_pid: label_to_pid,
-      graph: graph
-    } = _get_state(world)
-
+  @impl true
+  def handle_call({:get_neighbours, room}, _from, {graph, pid_to_label, label_to_pid}) do
     room_label = Map.get(pid_to_label, room)
     neighbours = []
-    for edge <- :digraph.out_edges(graph, room_label) do
+    neighbours = for edge <- :digraph.out_edges(graph, room_label) do
       {_,_,v2,_} = :digraph.edge(graph,edge)
       neighbour_pid = Map.get(label_to_pid, v2)
       [neighbour_pid | neighbours]
     end
-    neighbours
+    {:reply, neighbours, {graph, pid_to_label, label_to_pid}}
   end
 
-  @spec get_neighbours(world_pid, room_pid, atom) :: room_pid
-  def get_neighbours(world, room, direction) do
-    %{
-      pid_to_label: pid_to_label,
-      label_to_pid: label_to_pid,
-      graph: graph
-    } = _get_state(world)
-
+  @impl true
+  def handle_call({:get_neighbours, room, direction}, _from, {graph, pid_to_label, label_to_pid}) do
     room_label = Map.get(pid_to_label, room)
-    a = Enum.reduce(
+    neighbour = Enum.reduce(
       :digraph.out_edges(graph, room_label),
       nil,
       fn edge, acc ->
@@ -97,27 +79,6 @@ defmodule World do
         end
       end
     )
-  end
-
-  # Private helper functions
-
-  @spec _get_state(world_pid) :: State.t()
-  defp _get_state(world) do
-    Agent.get(world, & &1)
-  end
-
-  @spec _get_state(world_pid, key) :: state_attribute
-  defp _get_state(world, key) do
-    Agent.get(world, &Map.get(&1, key))
-  end
-
-  @spec _update_state(world_pid, key, state_attribute()) :: atom()
-  defp _update_state(world, key, value) do
-    Agent.update(world, &Map.put(&1, key, value))
-  end
-
-  @spec _update_state(world_pid, state_attribute()) :: atom()
-  defp _update_state(world, state) do
-    Agent.update(world, fn _ -> state end)
+    {:reply, neighbour, {graph, pid_to_label, label_to_pid}}
   end
 end
