1cfe55396922c56f928b4d300bc053397f7d2611
guidobotta <guido.botta@tiendanube.com>
2022-11-22 18:33:18 -0300

    Check if enemy and room exists and change drawer

--- apps/client/lib/client/drawer.ex
+++ apps/client/lib/client/drawer.ex
@@ -1,16 +1,19 @@
 defmodule Drawer do
-  def draw(game_state, cmd, err) do
+  def draw(game_state, arg) do
     IEx.Helpers.clear
     IO.puts("\n--- ESTADO DEL JUEGO ---\n")
     IO.inspect(game_state)
-    if cmd do
-      IO.puts("\n---- ULTIMO COMANDO ----\n")
-      IO.puts(cmd)
+
+    case arg do
+      {"cmd", command} ->
+        IO.puts("\n---- ULTIMO COMANDO ----\n")
+        IO.puts(command)
+      {"err", error} ->
+        IO.puts("\n---- ERROR ----\n")
+        IO.puts(error)
+      _ -> ""
     end
-    if err do
-      IO.puts("\n---- ERROR ----\n")
-      IO.puts(err)
-    end
+
     IO.puts("\n------------------------\n")
   end
 end
