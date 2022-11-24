957ac13d03c4f5d1f3353261098e24cb8a98235c
Gianbelinche <gbelinche@fi.uba.ar>
2022-11-24 17:05:38 -0300

    Beatufy

--- apps/client/lib/client/drawer.ex
+++ apps/client/lib/client/drawer.ex
@@ -2,26 +2,33 @@ defmodule Drawer do
   def draw(game_state, arg) do
     IEx.Helpers.flush()
     IEx.Helpers.clear()
-    lines = "\n--- ESTADO DEL JUEGO ---\n" <> inspect(game_state)
-
-    lines =
-      lines <>
-        case arg do
-          {"cmd", command} ->
-            "\n---- ULTIMO COMANDO ----\n" <> inspect(command)
-
-          {"err", error} ->
-            "\n---- ERROR ----\n" <> inspect(error)
-
-          _ ->
-            ""
-        end <> "\n------------------------\n" <> _helper_text()
-
-    IO.puts(lines)
+    IO.puts(_draw_state(game_state))
   end
 
   defp _helper_text() do
     """
+    \nComandos:
+      attack <enemy>
+      move <direction>
+      exit
+
+    Ingresa un comando:
+    """
+  end
+
+  defp _draw_state(state) do
+    """
+    \n--- ESTADO DEL JUEGO ---
+    \nEnemigos:
+    #{Enum.map(state.enemies, fn e -> "#{e.id} #{e.health}" end)}
+    \nJugadores:
+    #{Enum.map(state.players, fn p -> "#{p.id} #{p.health}" end)}
+    \nTurno:
+    #{state.turn}
+    \nDestinos:
+    #{Enum.map(state.rooms, fn r -> "#{r}" end)}
+    \nJugador:
+    #{state.player.id} #{state.player.health}
     \nComandos:
       attack <enemy>
       move <direction>
