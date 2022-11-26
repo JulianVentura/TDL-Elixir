0742526fde1077260261544b893e199efa95fe56
FrancoBar <fbarreneche@fi.uba.ar>
2022-11-25 22:46:52 -0300

    Add player/enemy table

--- apps/client/lib/client/drawer.ex
+++ apps/client/lib/client/drawer.ex
@@ -4,33 +4,45 @@ defmodule Drawer do
     IO.puts(_draw_state(game_state))
   end
 
+  defp _parse_stance(stance) do
+    case stance do
+      :fire -> IO.ANSI.format([:red, :bright, "F"])
+      :water -> IO.ANSI.format([:blue, :bright, "A"])
+      :plant -> IO.ANSI.format([:green, :bright, "P"])
+      _ -> "-"
+    end
+  end
+
+  defp _parse_entity(entity) do
+    if entity do
+      id = String.pad_trailing("#{entity.id}", 10)
+      health = String.pad_trailing("#{entity.health}", 4)
+      stance = _parse_stance(entity.stance)
+      "#{id} #{health} #{stance} "
+
+    else
+      String.pad_trailing("", 18)
+    end
+  end
+
+  defp _parse_entities(players, enemies) do
+    "/#{String.pad_trailing("", 38, "-")}\\\n" <>
+    "|#{String.pad_trailing("JUGADORES", 18)}| #{String.pad_trailing("ENEMIGOS", 18)}|\n" <>
+    "|#{String.pad_trailing("", 38, "-")}|\n" <>
+    List.to_string(for i <- 0..max(length(players), length(enemies)) do
+      p = if i < length(players) do Enum.at(players, i) else nil end
+      e = if i < length(enemies) do Enum.at(enemies, i) else nil end
+      "|#{_parse_entity(p)}| #{_parse_entity(e)}|\n"
+    end) <>
+    "\\#{String.pad_trailing("", 38, "-")}/\n"
+  end
+
   defp _draw_state(state) do
-    String.trim(
       """
-      \n--- ESTADO DEL JUEGO ---
-      #{format_enemy("\nEnemigos:")}
-      #{if Enum.empty?(state.enemies) do
-        format_enemy("No hay enemigos\n")
-      else
-        format_enemy(Enum.map(state.enemies, fn e -> "#{e.id} Vida: #{e.health} Tipo: #{e.stance} \n" end))
-      end}
-      """,
-      "\n"
-    ) <>
-      String.trim(
-        """
-        #{format_player("\nJugadores:")}
-        #{format_player(Enum.map(state.players, fn p -> "#{p.id} Vida: #{p.health} Tipo: #{p.stance} \n" end))}
-        """,
-        "\n"
-      ) <>
-      """
-      #{format_turn("\nTurno:")}
-      #{format_turn(state.turn)}
-      #{format_room("\nDestinos:")}
-      #{format_room(Enum.map(state.rooms, fn r -> "#{r} " end))}
-      #{format_player("\nTu Jugador:")}
-      #{format_player("#{state.player.id} Vida: #{state.player.health} Tipo: #{state.player.stance}")}
+      #{format_turn("Turno: #{state.turn}")}
+      #{_parse_entities(state.players, state.enemies)}
+      #{format_room("Destinos: #{Enum.map(state.rooms, fn r -> "#{r} " end)}")}
+      #{format_player("Tu Jugador: #{_parse_entity(state.player)}")}
       \nComandos:
         attack <enemy>
         move <direction>
