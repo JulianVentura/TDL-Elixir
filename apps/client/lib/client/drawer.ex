e64738c4b7048633386bd41bb5216b7805bd8fad
guidobotta <guido.botta@tiendanube.com>
2022-11-27 21:36:27 -0300

    Change types to emojis and enemy to red

--- apps/client/lib/client/drawer.ex
+++ apps/client/lib/client/drawer.ex
@@ -1,5 +1,6 @@
 defmodule Drawer do
-  def draw(game_state, arg) do
+
+  def draw(game_state) do
     IEx.Helpers.clear()
     _draw_state(game_state)
   end
@@ -10,9 +11,9 @@ defmodule Drawer do
 
   defp _parse_stance(stance) do
     case stance do
-      :fire -> _format_color(:red, "F")
-      :water -> _format_color(:blue, "A")
-      :plant -> _format_color(:green, "P")
+      :fire -> "🔥"
+      :water -> "💧"
+      :plant -> "🌱"
       _ -> "-"
     end
   end
@@ -25,12 +26,12 @@ defmodule Drawer do
       "#{id} #{health} #{stance} "
 
     else
-      String.pad_trailing("", 18)
+      String.pad_trailing("", 19)
     end
   end
 
   defp _fill(left, middle, right, fill) do
-    "#{left}#{String.pad_trailing("", 18, fill)}#{middle}#{String.pad_trailing("", 19, fill)}#{right}\n"
+    "#{left}#{String.pad_trailing("", 19, fill)}#{middle}#{String.pad_trailing("", 20, fill)}#{right}\n"
   end
 
   defp _color_turn(turn_id, entity) do
@@ -44,12 +45,12 @@ defmodule Drawer do
 
   defp _parse_entities(players, enemies, player_id, turn_id) do
     _fill("┌", "┬", "┐","─") <>
-    "│#{String.pad_trailing("JUGADORES", 18)}│ #{String.pad_trailing("ENEMIGOS", 18)}│\n" <>
+    "│#{String.pad_trailing("JUGADORES", 19)}│ #{String.pad_trailing("ENEMIGOS", 19)}│\n" <>
     _fill("├", "┼", "┤","─") <>
     List.to_string(for i <- 0..max(length(players), length(enemies)) do
       p = if i < length(players) do Enum.at(players, i) else nil end
       e = if i < length(enemies) do Enum.at(enemies, i) else nil end
-      parsed_e = _color_turn(turn_id, e)
+      parsed_e = _format_color(:red, _color_turn(turn_id, e))
       parsed_p = _color_turn(turn_id, p)
       parsed_p = _color_is_player(player_id, p, parsed_p)
       "│#{parsed_p}│ #{parsed_e}│\n"
@@ -59,19 +60,19 @@ defmodule Drawer do
 
   defp _draw_state(state) do
       IO.write(_parse_entities(state.players, state.enemies, state.player.id, state.turn))
-      
+
       if Enum.empty?(state.enemies) do
         IO.puts(" DESTINOS: " <> _format_color(:blue, "#{Enum.map(state.rooms, fn r -> "#{r} " end)}"))
       end
-      
+
       if state.turn in Enum.map(state.enemies, fn e -> e.id end) do
-        :timer.sleep(1500)
+        :timer.sleep(Application.get_env(:client, :time_to_draw))
       end
-      
+
       IO.write("\n> ")
   end
 
   def draw_msg(msg) do
-    IO.puts(msg)
+    IO.write(msg <> "\n> ")
   end
 end
